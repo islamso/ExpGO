f189cd14294b742792c471a31c7b2a7de76f14f6
pgeogheg <pgeogheg@tcd.ie>
2016-02-19 10:01:28 +0000

    added some jQuery functions to back end. Current version of business sign up sheet produces some errors

--- FrontEnd/backEndJQuery.js
+++ FrontEnd/backEndJQuery.js
@@ -1,13 +1,41 @@
 $(document).ready(function() {
-	$("#passField").click(function(){
+	document.addEventListener("keydown", KeyCheck);
+	function KeyCheck(event) {
+	   var KeyID = event.keyCode;
+	   switch(KeyID) {
+	      case 8:
+	      	passCheck();
+	      	break; 
+	      default:
+	      	break;
+	   }
+	};
+
+	$("#passField").keypress(function(){
 		console.log("test");
-		console.log($(this).val());
-		var passLength = $(this).val();
+		passCheck();
+		// var passLength = $(this).val().length;
+		// if (passLength <= 5) {
+		// 	$(this).css("border-color", "#FF0000");
+		// 	$(this).css("border-style", "solid");
+		// 	$(this).css("border-width", "5px");
+		// } else {
+		// 	$(this).css("border-color", "#00FF00");
+		// 	$(this).css("border-style", "#FF0000");
+		// 	$(this).css("border-width", "5px");
+		// }
+	});
+
+	function passCheck() {
+		var passLength = $(this).val().length;
 		if (passLength <= 5) {
-			console.log("too short");
 			$(this).css("border-color", "#FF0000");
+			$(this).css("border-style", "solid");
+			$(this).css("border-width", "5px");
 		} else {
-			$(this).css("border-color", "#0000FF");
+			$(this).css("border-color", "#00FF00");
+			$(this).css("border-style", "#FF0000");
+			$(this).css("border-width", "5px");
 		}
-	})
+	};
 });
