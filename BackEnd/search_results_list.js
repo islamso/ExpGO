86333eae7ea494ad9e4006cbbfe255b1bfc7920a
pgeogheg <pgeogheg@tcd.ie>
2016-03-11 12:21:18 +0000

    :poop: Added view button to search results

--- BackEnd/search_results_list.js
+++ BackEnd/search_results_list.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$('.view-more').click(function() {
-		$(this).parent().find('.list-group-item-text').toggle(500);
+		$(this).parent().find('.list-group-item-details').toggle(500);
 	});
 });
