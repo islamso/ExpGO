70bff4d612bfd853b7861e5a6d78b1b62faef8b6
Pippa Gilsenan <pgilsena@tcd.ie>
2016-03-24 01:06:45 +0000

    Working sign up profile

--- BackEnd/signup_stu.php
+++ BackEnd/signup_stu.php
@@ -1,49 +1,62 @@
 <?php
-	$dbhost="mysql6.000webhost.com";
-	$dbuser="a5349876_userMan";
-	$dbpass="UMpass61";
-	$dbname="a5349876_users";
-	
-	try {
-		$conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
-    	// set the PDO error mode to exception
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$dbhost="mysql6.000webhost.com";
+$dbuser="a5349876_userMan";
+$dbpass="UMpass61";
+$dbname="a5349876_users";
 
-    if (! get_magic_quotes_gpc())
-    {
-        $first_name = addslashes($_POST['fName']);
-        $last_name = addslashes($_POST['lName']);
-        $email = addslashes($_POST['email']);
-        $password = addslashes($_POST['pass']);
-        $university = addslashes($_POST['college']);
-        $year = addslashes($_POST['year']);
-        $course = addslashes($_POST['course']);
-    }
-    else
-    {
-        $first_name = $_POST['fName'];
-        $last_name = $_POST['lName'];
-        $email = $_POST['email'];
-        $password = $_POST['pass'];
-        $university = $_POST['college'];
-        $year = $_POST['year'];
-        $course = $_POST['course'];
-    }
-    	
-    	// INSERT VALUES
-    	$sql = "INSERT INTO students (firstName,lastName, email,password,university,year,course)
-    	VALUES ('$first_name', '$last_name', '$email','$password','$university','$year','$course')";
-    
-    	// use exec() because no results are returned
-    	$conn->exec($sql);
-    	echo "New record created successfully";
-    }
-	catch(PDOException $e)
-    {
-    	echo $sql . "<br>" . $e->getMessage();
-	}
-	//$conn = null;
-    session_register($username);
-    session_register($password);
-    header('Location: http://expgo.gq/student_profile.html');
+try {
+  $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+        // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  if (! get_magic_quotes_gpc())
+  {
+    $firstName = addslashes($_POST['fName']);
+    $lastName = addslashes($_POST['lName']);
+    $email = addslashes($_POST['email']);
+    $password = addslashes($_POST['pass']);
+    $uni = addslashes($_POST['college']);
+    $year = addslashes($_POST['year']);
+    $course = addslashes($_POST['course']);
+}
+else
+{
+    $firstName = $_POST['fName'];
+    $lastName = $_POST['lName'];
+    $email = $_POST['email'];
+    $password = $_POST['pass'];
+    $uni = $_POST['college'];
+    $year = $_POST['year'];
+    $course = $_POST['course'];
+}
+
+        // INSERT VALUES
+$sql = "INSERT INTO students (firstName,lastName, email,password,university,year,course)
+VALUES ('$firstName', '$lastName', '$email','$password','$uni','$year','$course')";
+
+        // use exec() because no results are returned
+$conn->exec($sql);
+echo "New record created successfully";
+}
+catch(PDOException $e)
+{
+   echo $sql . "<br>" . $e->getMessage();
+}
+    //$conn = null;
+session_register($username);
+$_SESSION["username"] = $username;
+session_register($password);
+$_SESSION["password"] = $password;
+session_register($firstName);
+$_SESSION["firstName"] = $firstName;
+session_register($lastName);
+$_SESSION["lastName"] = $lastName;
+session_register($uni); 
+$_SESSION["univ"] = $uni;
+session_register($year);
+$_SESSION["year"] = $year;
+//session_register($bio);
+session_register($course);
+$_SESSION["course"] = $course;
+header("location:../student_profile.html");
 ?>
